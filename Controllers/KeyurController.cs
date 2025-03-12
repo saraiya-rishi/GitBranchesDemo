a3b181238f05c40c38194a6f68e203c8bf9d11be
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-03-12 21:31:56 +0530

    keyur push

---
+++ Controllers/KeyurController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitBranchesDemo.Controllers
+{
+    public class KeyurController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
