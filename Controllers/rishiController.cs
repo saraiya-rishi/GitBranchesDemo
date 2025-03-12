e92780a13c5400ca465f99dc29eed11a19fbaae0
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-03-12 21:32:45 +0530

    rishi push

---
+++ Controllers/rishiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitBranchesDemo.Controllers
+{
+    public class rishiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
